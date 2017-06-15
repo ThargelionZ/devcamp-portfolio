cfd0312dd29b14dd173d2137ee24c007cc9ae2c4
ThargelionZ <measles10@gmail.com>
2017-06-15 17:32:08 +0000

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,8 +30,8 @@ puts "5 Skills Created"
     title: "Portfolio Title: #{portfolio_item}",
     subtitle: "My Great Service",
     body: "Bacon ipsum dolor amet kevin bacon andouille corned beef tri-tip pork belly pork ball tip sirloin salami pastrami tongue porchetta ground round beef ribs. Flank beef capicola turkey tongue. Turducken flank meatball, ground round pastrami strip steak kevin landjaeger picanha. Tri-tip chicken boudin rump landjaeger pork chop salami pancetta turkey cupim kevin brisket cow prosciutto venison. Chuck tongue ham capicola leberkas filet mignon beef. Prosciutto shankle alcatra, boudin cupim turducken brisket ham hock capicola pastrami pork. Ball tip biltong andouille, jowl ground round venison spare ribs pork chop chuck pork loin.",
-    main_image: "https://placeholder.com/600x400",
-    thumb_image: "https://placeholder.com/350x200"
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
   )
 end
 
