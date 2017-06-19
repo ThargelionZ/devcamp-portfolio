0103254be90961eb709e9a7f408b64d862147ab4
ThargelionZ <measles10@gmail.com>
2017-06-19 21:15:54 +0000

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do |topic|
+  Topic.create!(title: "Topic #{topic}")
+end
 
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Bacon ipsum dolor amet kevin bacon andouille corned beef tri-tip pork belly pork ball tip sirloin salami pastrami tongue porchetta ground round beef ribs. Flank beef capicola turkey tongue. Turducken flank meatball, ground round pastrami strip steak kevin landjaeger picanha. Tri-tip chicken boudin rump landjaeger pork chop salami pancetta turkey cupim kevin brisket cow prosciutto venison. Chuck tongue ham capicola leberkas filet mignon beef. Prosciutto shankle alcatra, boudin cupim turducken brisket ham hock capicola pastrami pork. Ball tip biltong andouille, jowl ground round venison spare ribs pork chop chuck pork loin."
+    body: "Bacon ipsum dolor amet kevin bacon andouille corned beef tri-tip pork belly pork ball tip sirloin salami pastrami tongue porchetta ground round beef ribs. Flank beef capicola turkey tongue. Turducken flank meatball, ground round pastrami strip steak kevin landjaeger picanha. Tri-tip chicken boudin rump landjaeger pork chop salami pancetta turkey cupim kevin brisket cow prosciutto venison. Chuck tongue ham capicola leberkas filet mignon beef. Prosciutto shankle alcatra, boudin cupim turducken brisket ham hock capicola pastrami pork. Ball tip biltong andouille, jowl ground round venison spare ribs pork chop chuck pork loin.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -25,10 +29,20 @@ end
 
 puts "5 Skills Created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio Title: #{portfolio_item}",
-    subtitle: "My Great Service",
+    subtitle: "Ruby on Rails",
+    body: "Bacon ipsum dolor amet kevin bacon andouille corned beef tri-tip pork belly pork ball tip sirloin salami pastrami tongue porchetta ground round beef ribs. Flank beef capicola turkey tongue. Turducken flank meatball, ground round pastrami strip steak kevin landjaeger picanha. Tri-tip chicken boudin rump landjaeger pork chop salami pancetta turkey cupim kevin brisket cow prosciutto venison. Chuck tongue ham capicola leberkas filet mignon beef. Prosciutto shankle alcatra, boudin cupim turducken brisket ham hock capicola pastrami pork. Ball tip biltong andouille, jowl ground round venison spare ribs pork chop chuck pork loin.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio Title: #{portfolio_item}",
+    subtitle: "Angular",
     body: "Bacon ipsum dolor amet kevin bacon andouille corned beef tri-tip pork belly pork ball tip sirloin salami pastrami tongue porchetta ground round beef ribs. Flank beef capicola turkey tongue. Turducken flank meatball, ground round pastrami strip steak kevin landjaeger picanha. Tri-tip chicken boudin rump landjaeger pork chop salami pancetta turkey cupim kevin brisket cow prosciutto venison. Chuck tongue ham capicola leberkas filet mignon beef. Prosciutto shankle alcatra, boudin cupim turducken brisket ham hock capicola pastrami pork. Ball tip biltong andouille, jowl ground round venison spare ribs pork chop chuck pork loin.",
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x200"
