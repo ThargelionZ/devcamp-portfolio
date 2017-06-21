0ef80077c47763861e702c0b63aeb33691239478
ThargelionZ <measles10@gmail.com>
2017-06-21 20:51:34 +0000

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   # Must Confirm, Ability to lock user out, Can timeout user, Integrate third-party logins
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates_presence_of :name
+  
+  def first_name
+    self.name.split.first
+  end
+  
+  def last_name
+    self.name.split.last
+  end
 end
