8694b4782c8fff1f5ffb2dd55d7908b120f3b1df
ThargelionZ <measles10@gmail.com>
2017-06-27 20:57:02 +0000

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    MylesparkerViewTool::Renderer.copyright "Myles Parker", "All Rights Reserved"
+  end
 end
